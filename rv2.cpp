a94cf51893de1428466ad3b9a452012f2a6bd105
NeuralCoder18 <neuralcipher1918@gmail.com>
2025-12-27 18:55:14 +0530

    Sort Color

---
+++ rv2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else { 
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements (only 0, 1, 2): ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    sortColors(nums);
+
+    cout << "Sorted array: ";
+    for (int x : nums) {
+        cout << x << " ";
+    }
+
+    return 0;
+}
